008293ed3d5bf5a184962381a94970367e541d78
Kim Ward <kmewrd@outlook.com>
2022-04-20 10:06:58 -0500

    Add endpoint for POSTing a sighting

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const fungi = require('./data.js')
+const fungi = require('./data.js');
+
+app.use(express.json());
 
 app.set('port', process.env.PORT || 3001);
 app.locals.title = 'U.F.O. - Unidentified Fungus Outdoors';
@@ -14,6 +16,14 @@ app.get('/api/v1/fungi/:region', (request, response) => {
   const regionalFungi = app.locals.fungi.filter(fungus => fungus.regions.includes(request.params.region));
 
   response.json(regionalFungi);
+});
+
+app.post('/api/v1/sightings', (request, response) => {
+  if (request.body.id && request.body.date && request.body.location && request.body.notes) {
+    response.json(request.body);
+  } else {
+    response.sendStatus(422);
+  }
 });
 
 app.listen(app.get('port'), () => {
